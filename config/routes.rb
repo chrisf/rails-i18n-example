aaf5f9ebb247c0c5de46173989b019f8fed1ecae
Chris Fitzmorris <cfitz93@gmail.com>
2018-02-09 14:42:25 -0600

    Add sample english/spanish translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'hello/index'
+  root 'hello#index'
+
+  scope "(:locale)", locale: /en|es/ do
+    get 'hello/index'
+    get 'hello/about'
+    # resources :books
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
